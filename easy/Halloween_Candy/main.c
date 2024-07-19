557c9a9d191db855a516344dabedbc57f51f0eb6
happ-2h <hikonetgames@gmail.com>
2024-07-19 16:11:16 -0700

    Halloween Candy C solution

---
+++ easy/Halloween_Candy/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int nHouses = 0;
+
+  scanf("%d", &nHouses);
+
+  /*
+    Only 2 houses give dollar bills
+      2 / nHouses
+
+    Convert the percentage to decimal
+      (2 / nHouses) * 100 => 200 / nHouses
+   */
+  fprintf(stdin, "%d", (int)ceil(200.0f / nHouses));
+
+  return 0;
+}
