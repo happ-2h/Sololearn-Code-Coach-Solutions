f3497282b517bacab8c176c94860559a61095cad
happ-2h <hikonetgames@gmail.com>
2024-07-19 17:47:01 -0700

    Fruit Bowl C solution

---
+++ easy/Fruit_Bowl/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int nFruit = 0;
+
+  scanf("%d", &nFruit);
+
+  /*
+    Half are apples
+      nFruit / 2
+
+    You need 3 apples to make 1 pie
+    Calculate how many pies you can make
+      nFruit / 2 / 3 => nFruit / 6
+   */
+  fprintf(stdin, "%d", nFruit / 6);
+
+  return 0;
+}
