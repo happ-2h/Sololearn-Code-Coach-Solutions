adf3641ec002e297bbe5d9318aca61ee97429dd1
happ-2h <hikonetgames@gmail.com>
2024-07-19 18:05:28 -0700

    Cheer Creator C solution

---
+++ easy/Cheer_Creator/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int yards = 0;
+
+  scanf("%d", &yards);
+
+  // Print cheer depending on yards gained
+  if (yards > 10)     puts("High Five");
+  else if (yards > 1) puts("shh");
+  else while(yards--) puts("Ra!");
+
+  return 0;
+}
