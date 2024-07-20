7ed59d447a514b410ac792b5e2f1682da675b60b
happ-2h <hikonetgames@gmail.com>
2024-07-19 18:12:20 -0700

    Skee-Ball C solution

---
+++ easy/Skee-Ball/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int pts  = 0;
+  int cost = 0;
+
+  scanf("%d %d", &pts, &cost);
+
+  fprintf(stdin, "%s",
+    pts / 12 >= cost ? // Did you get enough tickets ?
+      "Buy it!" :      // Yes, buy it
+      "Try again"      // No, keep on trying
+  );
+
+  return 0;
+}
