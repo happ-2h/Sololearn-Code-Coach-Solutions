cb6641bc9655e1668e067ae81cebd6cd9948130f
happ-2h <hikonetgames@gmail.com>
2024-07-19 21:08:19 -0700

    Izzy the Iguana C solution

---
+++ easy/Izzy_the_Iguana/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#define STRLEN_MAX 0xFF
+
+int main(int argc, char const *argv[]) {
+  char snacks[STRLEN_MAX];
+  int snackPts = 0;
+
+  fgets(snacks, STRLEN_MAX, stdin);
+
+  // Replace newline with null terminator
+  char* pch = strrchr(snacks, '\n');
+  if (pch) *pch = '\0';
+
+  pch = strtok(snacks, " ");
+
+  while(pch) {
+    // Add up all the points
+    if      (strncmp(pch, "Lettuce", 8) == 0) snackPts += 5;
+    else if (strncmp(pch, "Carrot",  8) == 0) snackPts += 4;
+    else if (strncmp(pch, "Mango",   8) == 0) snackPts += 9;
+
+    pch = strtok(NULL, " ");
+  }
+
+  fprintf(stdout, "%s",
+    snackPts >= 10 ?    // Do we have the right snacks?
+      "Come on Down!" : // Yes, the iguana comes down right away
+      "Time to wait"    // No, it will take its time to come down
+  );
+
+  return 0;
+}
