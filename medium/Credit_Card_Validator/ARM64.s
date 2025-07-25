1d795e97937ddb5a4fa9cbb5274e2df8ae7ae59a
happ-2h <hikonetgames@gmail.com>
2025-07-24 20:01:44 -0700

    Credit Card Validator AArch64 solution

---
+++ medium/Credit_Card_Validator/ARM64.s
@@ -0,0 +1,129 @@
+/*
+  Compiled and ran on Qualcomm Snapdragon 865
+ */
+  .global _start
+
+_start:
+  // Get CCN
+  mov w0, #0
+  ldr x1, =ccn
+  mov w2, #32
+  mov w8, #63
+  svc 0
+
+  mov x0, x1
+  bl strlen
+
+  // CCN must be 16 characters
+  cmp x0, #16
+  b.ne not_valid
+
+  // Convert characters to integers
+  ldr x0, =ccn
+  ldr x1, =nums
+  mov w5, #16
+convert:
+  ldrb w2, [x0], #1
+  sub  w3, w2, #'0'
+  // Character is not '0' - '9'
+  cmp w3, #10
+  b.ge not_valid
+
+  // Save number
+  str w3, [x1], #4
+
+  subs w5, w5, #1
+  b.ne convert
+
+  // Multiply every second digit by 2
+  ldr x0, =nums
+  mov w1, #16   // Start from back
+mul_by_2:
+  subs w1, w1, #2
+  b.mi add_all
+
+  ldr  w2, [x0, x1, lsl #2]
+  lsl  w2, w2, #1
+
+  // If number > 9, wrap number
+  subs w5, w2, #9
+  csel w2, w5, w2, pl
+
+  str  w2, [x0, x1, lsl #2]
+
+  b mul_by_2
+
+add_all:
+  ldr x0, =nums
+  mov w1, #0
+  mov w5, #0
+add_for:
+  ldr w2, [x0, x1, lsl #2]
+  add w5, w5, w2
+
+  add w1, w1, #1
+  cmp w1, #16
+  b.ne add_for
+
+mod_10:
+  mov  w0, #10
+  sdiv w1, w5, w0
+  mul  w1, w1, w0
+  subs w0, w5, w1
+  b.eq valid
+
+not_valid:
+  mov w0, #1
+  ldr x1, =str_not
+  mov w2, #4
+  mov w8, #64
+  svc 0
+
+valid:
+  mov w0, #0
+  ldr x1, =str_valid
+  mov w2, #5
+  mov w8, #64
+  svc 0
+
+exit:
+  mov w0, #0
+  mov w8, #93
+  svc 0
+
+/**
+ * @brief Counts the number of characters in a string
+ *
+ * @param[in]: Address of the string
+ *
+ * returns Number of characters in the provided string
+ */
+strlen:
+  stp x29, x30, [sp, #-16]!
+
+  mov w5, #0
+strlen_while:
+  ldrb w1, [x0], #1
+  // Exit on null terminator or newline
+  cmp w1, #0
+  b.eq strlen_exit
+  cmp w1, #'\n'
+  b.eq strlen_exit
+  // Exit when reached max string length
+  cmp w5, #32
+  b.eq strlen_exit
+
+  add w5, w5, #1
+
+  b strlen_while
+
+strlen_exit:
+  mov w0, w5
+  ldp x29, x30, [sp], #16
+  ret
+
+.data
+ccn:       .fill 32, 1, 0
+nums:      .fill 16, 4, 0
+str_not:   .asciz "not "
+str_valid: .asciz "valid"
