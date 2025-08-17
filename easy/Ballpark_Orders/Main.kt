52fae6d60290e548cecfcda6efa65db4b6c64ee3
happ-2h <hikonetgames@gmail.com>
2025-08-17 13:21:35 -0700

    Ballpark Orders Kotlin solution

---
+++ easy/Ballpark_Orders/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+  var total  = 0.0
+  val orders = readln()
+    .split(" ")
+    .forEach {
+      total += when(it) {
+        "Nachos", "Pizza" ->  6.0
+        "Cheeseburger"    -> 10.0
+        "Water"           ->  4.0
+        else              ->  5.0
+      }
+    }
+
+  println(String.format("%.2f", total * 1.07))
+}
